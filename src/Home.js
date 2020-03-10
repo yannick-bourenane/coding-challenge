ed9ab6bc7a8d075bd83bcab2ab442182087ff666
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-03-10 10:49:43 +0100

    First commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Hello, my name is ... and you are on my solution for the Ironhack
+        challenge!
+      </p>
+    </div>
+  );
+}
